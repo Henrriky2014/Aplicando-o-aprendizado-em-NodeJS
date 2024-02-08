dbb54e48e1e59e84befc466e7f764be270b9bacb
CH <carloshenrriky1@gmail.com>
2024-02-08 15:29:46 -0300

    Add database and folder migrations

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require("express-async-errors");
-
+const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError");
 
 const express = require("express");
@@ -11,6 +11,8 @@ app.use(express.json())
 
 
 app.use(routes);
+
+migrationsRun();
 
 app.use((error,request, response, next) => {
     if(error instanceof AppError) {
