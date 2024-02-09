afd638648a07452ce38ea26029d028874f0c355e
CH <carloshenrriky1@gmail.com>
2024-02-09 10:43:09 -0300

    Add knex library and creation of notes

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 const AppError = require("../utils/AppError");
 
@@ -21,6 +21,52 @@ class UsersController {
 
         return response.status(201).json();
 
+    }
+
+    async update(request,response) {
+        const { name, email, password, old_password} = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if (!user) {
+            throw new AppError("Usuário não encontrado!");
+        }
+
+        const userWhithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(userWhithUpdatedEmail && userWhithUpdatedEmail.id !== user.id) {
+            throw new AppError("E-mail ja cadastrado!");
+        }
+
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+        if(password && !old_password) {
+            throw new AppError("Voce precisa informar a senha antiga para definir a nova senha")
+        }
+
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password);
+
+            if (!checkOldPassword) {
+                throw new AppError("Senha antiga n confere!");
+            }
+
+            user.password = await hash(password,8);
+        }
+
+        await database.run(`
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            password=?,
+            updated_at = DATETIME('now')
+            WHERE id = ?`,
+            [user.name, user.email,user.password, id]);
+
+        return response.status(200).json();
     }
 }
 
